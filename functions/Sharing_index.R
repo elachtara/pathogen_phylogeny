a43998b06d1e6a34d404ad24d3963b37d4a530de
elachtara <elachtara21@amherst.edu>
2021-01-24 09:16:01 -0500

    computing sharing

---
+++ functions/Sharing_index.R
@@ -0,0 +1,40 @@
+
+data = fam2
+
+get_sharing <- function(data){
+sharing <- matrix(NA, nrow = 0, ncol = 3) %>% as.data.frame()
+colnames(sharing) <- c('org1', 'org2', 'percent')
+
+# Function to parse through and create pathogen sharing index
+for(i in 1:length(unique(data$host))){
+
+  # Hostnames 
+  hostnames <- unique(data$host)
+
+  # Pull the organism of comparison
+  org1 <- hostnames[i]
+  
+  # Pull the other organisms to compare
+  others <- c(1:length(hostnames))
+  others <- others[-i]
+
+  # Loop through the other organisms
+  for(j in others){
+    org2 <- hostnames[j]
+    
+    # Get total pathogens between them
+    total <- data %>% filter(host == c(org1, org2)) %>% select(sci_name, host) %>% group_by(sci_name, host) %>% unique()
+    
+    # Get the number of shared pathogens
+    shared <- total %>% group_by(sci_name) %>% count() %>% filter(n > 1) 
+    
+    # Get the percent shared between them
+    percent <- round(nrow(shared)/nrow(total), 3)
+    
+    # Bind together this comparison with all comparisons
+    temp <- cbind(org1, org2, percent)
+    sharing <- rbind(sharing, temp)
+  }
+}
+return(sharing)
+}
